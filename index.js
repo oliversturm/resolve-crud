987804e282022d3647302e65dc4ae0a136cbd7c8
Oliver Sturm <oliver@oliversturm.com>
2020-10-05 15:19:10 +0100

    initial

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  crudCommands: require('./commands').crudCommands,
+  crudAggregateProjections: require('./aggregate-projections').crudProjections,
+  crudReadModelProjections: require('./readmodel-projections').crudProjections,
+  crudResolvers: require('./resolvers').crudResolvers,
+};
